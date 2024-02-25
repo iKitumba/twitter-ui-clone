aec8998e3a87ee1e070eada9cd506c56468a091a
Alberto Kitumba <alberkitumba@gmail.com>
2024-02-25 18:29:35 +0100

    Add sidebar menu

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        twitter: "#1A8CD8",
+        twitterhover: "#1d9bf0",
       },
     },
   },
